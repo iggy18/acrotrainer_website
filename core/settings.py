3c306ac83d86a929520ef968017f3f9614dab6c1
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-02-18 20:05:29 -0800

    added a pytest plugin

--- core/settings.py
+++ core/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends',
         'NAME': env('DATABASE_NAME'),
         'USER': env('DATABASE_USER'),
         'PASSWORD':  env('DATABASE_PASSWORD'),
