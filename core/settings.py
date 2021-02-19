f44a77f8afb71baf421ac7c94895dc2a1b0e1a5c
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-02-19 13:10:34 -0800

    can you hear me now?

--- core/settings.py
+++ core/settings.py
@@ -29,10 +29,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+if sys.argv[1] == 'test':
+    SECRET_KEY = 11111111111111111111111111111111111111111111111111
+    
+else:    
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
