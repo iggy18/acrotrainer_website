cef22a1e96d8f8c889a22a6bc2b3b8e799042d46
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-02-19 13:03:12 -0800

    switched allowed hosts to all for now

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 
