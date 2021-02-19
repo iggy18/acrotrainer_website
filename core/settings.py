727d9d4f056c5c0a2e83347c3dea12fbd6af4df6
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-02-19 12:41:04 -0800

    changed workflow, checking if tests pass

--- core/settings.py
+++ core/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('DATABASE_NAME'),
         'USER': env('DATABASE_USER'),
-        'PASSWORD':  env('DATABASE_PASSWORD'),
-        'HOST':  env('DATABASE_HOST'),
-        'PORT':  env('DATABASE_PORT')
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT')
     }
 }
 
