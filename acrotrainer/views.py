2cc5108e9e7479a55bf4447d973d0c07348164b7
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-02-25 17:52:25 -0800

    cart succesfully renders items in cart from database, calcualtes totals according to price and quantity

--- acrotrainer/views.py
+++ acrotrainer/views.py
@@ -13,7 +13,8 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
-    context = {}
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
