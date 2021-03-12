b03c11bc1bf3a3679210dbfe98377b4bc1c7e889
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-03-11 18:40:32 -0800

    payment processing information returns to database

--- acrotrainer/views.py
+++ acrotrainer/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import Customer, Product, Order, OrderItem, ShippingAdress
+from .models import Customer, Product, Order, OrderItem, ShippingAddress
 from django.http import JsonResponse
 import json
+import datetime
 
 def store(request):
     if request.user.is_authenticated:
@@ -70,5 +71,28 @@ def updateItem(request):
     return JsonResponse('item has been added', safe=False)
 
 def processOrder(request):
-    print('data', request.body)
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    print(request.body)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.hide_shipping_address == True:
+            ShippingAddress.objects.create(
+                customer = customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            ) 
+    else:
+        print('user not authenticated')
     return JsonResponse('Payment complete!', safe=False)
